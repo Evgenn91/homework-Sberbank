9c5f8d4ed85701f1326374e1a5aa2352f1182baa
Evgeniy Nikulin <evgeniy.nikulin.1991@mail.com>
2021-09-02 15:41:15 +0500

    refactoring Task 3.5

--- 3 Task/src/eFiveGetWinnerGame/SearchWinGame.java
+++ 3 Task/src/eFiveGetWinnerGame/SearchWinGame.java
@@ -1,29 +1,41 @@
 package eFiveGetWinnerGame;
 
 
-import java.util.Arrays;
-import java.util.Objects;
+import java.util.*;
 
 public class SearchWinGame {
     private String[] listForSplit;
-    private Integer maxPoints = 0;
+    Map<String,Integer> listPlayers;
 
     public SearchWinGame(String[] listForSplit) {
         this.listForSplit = listForSplit;
-        findMaxPoints();
+        this.listPlayers = new HashMap();
     }
 
     public void printWinner(){
         if(listForSplit.length!=0) {
             try {
+                int maxPoints = 0;
+                String nameWin ="";
                 for(String player: listForSplit){
                     String name = player.split(" ")[0];
                     Integer points = Integer.parseInt(player.split(" ")[1]);
-                    if(points==this.maxPoints) {
-                        System.out.println("Игрок набравший максимальное колличество очков: "+name+", набравший "+ points+" очков.");
-                        break;
+                    if(listPlayers.containsKey(name)){
+                        int pointsPlayer = points+listPlayers.get(name);
+                        listPlayers.put(name, pointsPlayer);
+
+                        if(pointsPlayer>maxPoints) {
+                            maxPoints = pointsPlayer;
+                            nameWin=name;
+                        }
+                    } else {
+                        listPlayers.put(name,points);
+                        if(points>maxPoints) maxPoints = points;
                     }
+
                 }
+                System.out.println("Победитель игры: "+nameWin+", быстрее всех набравший "+maxPoints+" очков." );
+
 
             } catch (ArrayIndexOutOfBoundsException e){
                 System.out.println("Вы ввели некорректные данные.");
@@ -34,39 +46,21 @@ public class SearchWinGame {
 
         }
     }
-    //находим максимальное колличество очков в игре
-    private void findMaxPoints(){
-        if(listForSplit.length!=0) {
-            try {
-                for(String player: listForSplit){
-                    Integer points = Integer.parseInt(player.split(" ")[1]);
-                    if(points>this.maxPoints) this.maxPoints=points;
-                }
 
-            } catch (ArrayIndexOutOfBoundsException e){
-                System.out.println("Вы ввели некорректные данные.");
-                System.out.println("Формат вводимых данных: имя_игрока количество очков" );
-                System.out.println("Проверьте ваши данные и попробуйте снова" );
-                throw e;
-            }
-        }
-
-    }
 
     public String[] getListForSplit() {
         return listForSplit;
     }
 
-    public Integer getMaxPoints() {
-        return maxPoints;
+    public Map<String, Integer> getListPlayers() {
+        return listPlayers;
     }
 
     @Override
     public String toString() {
         return "SearchWinGame{" +
                 "listForSplit=" + Arrays.toString(listForSplit) +
-                ", maxPoints=" + maxPoints +
+                ", listPlayers=" + listPlayers +
                 '}';
     }
-
 }
